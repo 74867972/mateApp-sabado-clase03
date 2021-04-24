78f43bc20c9fc4f68db5c9b09869abb9e1cd388f
74867972 <74867972@certus.edu.pe>
2021-04-24 10:24:12 -0500

    Create tarea.py

---
+++ tarea.py
@@ -0,0 +1,31 @@
+print("Hola Mundo")
+print("Brahan Carbajal")
+
+#suma
+a = 12
+b = 14
+suma = a + b
+print("la suma es",suma)
+
+# Operaciones con STRING.
+str1 = 'a'
+str2 = 'b'
+resultado = str1 + str2
+print(resultado)
+print(resultado * 5)
+
+#OPERACIONES EN INCREMENTO
+a=23
+print("a=23  -->",a)
+a=a+1
+print("a=a+1 -->",a)
+a+=1
+print("a+=1  -->",a)
+a*=2
+print("a*=2 -->" ,a)
+a/=2
+print("a/=2 -->",a)
+a-=2
+print("a-=2 -->",a)
+a**=2
+print("a**=2 -->",a)
